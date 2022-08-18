85828437b69d857815ddd74e0331681e0b1949e3
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2022-08-18 17:24:43 -0500

    añadir entradas a portada

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def home(request):
     #return HttpResponse("Bienvenid@ a mi blog") #Texto plano
-    return render(request,"blog/home.html")
+    posts = Post.objects.all()
+    return render(request,"blog/home.html",{'posts':posts})
