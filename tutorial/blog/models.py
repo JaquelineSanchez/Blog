e28ad4f094c216be0e058edda8392b9900c111f5
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2022-08-18 16:41:42 -0500

    agregar modelo al administrador

--- tutorial/blog/models.py
+++ tutorial/blog/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200, verbose_name = "Titulo")
+    content = models.TextField(verbose_name = "Contenido")
+
+    def __str__(self):
+        return self.title
