40626b4033d459f67707a1ded3ace475aeb90893
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2022-08-18 17:13:12 -0500

    crear vista para Portada

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return HttpResponse("Bienvenid@ a mi blog") #Texto plano
+    return render(request,"blog/home.html")
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import home
 
 urlpatterns = [
+    path('',home),  #Vista de portada
     path('admin/', admin.site.urls),
 ]
